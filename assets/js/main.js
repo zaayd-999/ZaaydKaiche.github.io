dd0d102dd3f9e79ffe8580c2fc330c04adb03258
zaayd_999 <z.kaiche@edu.umi.ac.ma>
2025-08-17 01:47:02 +0100

    Portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,111 @@
+function clearAllActive() {
+    for (let selector of document.querySelectorAll(".active")) {
+        selector.removeAttribute("class", "active");
+    }
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+            clearAllActive();
+            this.setAttribute("class", "active");
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('nav a');
+    highlightCurrentSection();
+    window.addEventListener('scroll', highlightCurrentSection);
+    function highlightCurrentSection() {
+        let current = '';
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+
+            if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
+                current = section.getAttribute('id');
+                if (current == "about2") current = "about";
+            }
+        });
+
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+            if (link.getAttribute('href') === `#${current}`) {
+                link.classList.add('active');
+            }
+        });
+    }
+});
+
+
+const langToggle = document.getElementById('langToggle');
+let currentLang = document.documentElement.lang;
+
+function initializeContent() {
+    currentLang = document.documentElement.lang;
+    document.querySelectorAll('[data-tr]').forEach(element => {
+        const key = element.getAttribute('data-tr');
+        element.textContent = translations[currentLang][key];
+    });
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    document.querySelector('.hero h1').textContent = translations[currentLang].heroTitle;
+    document.querySelector('.hero p').textContent = translations[currentLang].heroSubTitle;
+    document.querySelector('#about h2').textContent = translations[currentLang].aboutTitle;
+    document.querySelector('.about-text p').textContent = translations[currentLang].aboutText;
+    document.querySelector('#skills h2').textContent = translations[currentLang].skillsTitle;
+    document.querySelector('#projects h2').textContent = translations[currentLang].projectsTitle;
+    document.querySelector('footer p').innerHTML = `${translations[currentLang].copyright} &copy; ${new Date().getFullYear()} ZAAYD KAICHE`;
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    let about2Skillsp = document.querySelectorAll('#about2.skills p');
+    let about2Skillsh3 = document.querySelectorAll('#about2.skills h3');
+    let i = 0;
+    for(let i = 0 ; i< about2Skillsp.length ; i++){
+        about2Skillsp[i].textContent = translations[currentLang].aboutStats[i].value;
+    }
+    i=0;
+    for(let i = 0 ; i< about2Skillsh3.length ; i++){
+        about2Skillsh3[i].textContent = translations[currentLang].aboutStats[i].title;
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    let skillsp = document.querySelectorAll("#skills .skill-card p");
+    let skillsh3 = document.querySelectorAll("#skills .skill-card h3");
+    i = 0;
+    for(let i = 0 ; i< skillsp.length ; i++){
+        skillsp[i].textContent = translations[currentLang].skillsCards[i].description;
+    }
+    i=0;
+    for(let i = 0 ; i< about2Skillsh3.length ; i++){
+        skillsh3[i].textContent = translations[currentLang].skillsCards[i].title;
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    const projectsGrid = document.querySelector('.projects-grid');
+    projectsGrid.innerHTML = translations[currentLang].projectsCards.map(project => `
+            <div class="project-card">
+                <img src="${project.image}" alt="${project.title}">
+                <h3>${project.title}</h3>
+                <p>${project.description}</p>
+                <a ${project.link ? `href="${project.link}" target="_blank"` : ``} class="project-link ${project.link ? `` : `dis-btn`}">${project.link ? translations[currentLang].projectLinkText : translations[currentLang].projectFixText}</a>
+            </div>
+    `).join('');
+}
+document.addEventListener('DOMContentLoaded', initializeContent);
+
+function updateLanguage() {
+    document.documentElement.lang = (document.documentElement.lang == "en" ? "fr" : "en");
+    initializeContent();
+}
+
+document.getElementById('langToggle').addEventListener('click', () => {
+    updateLanguage();
+    document.getElementById('langToggle').innerHTML = `<i class="fas fa-globe"></i>${document.documentElement.lang.toUpperCase()}`;
+});
---
+++ assets/js/translations.js
@@ -0,0 +1,126 @@
+const translations = {
+    en : {
+        home: "Home",
+        about: "About Me",
+        skills: "My Skills",
+        projects: "My Projects",
+        heroTitle : "Hello, I'm KAICHE ZAAYD",
+        heroSubTitle : "Future Software engenieer.",
+        //About Me
+        aboutTitle : "About Me",
+        aboutText : `I am a backend developer and student at ENSAM Meknès with a strong focus on building efficient and scalable solutions. My experience includes working with Node.js, MongoDB, MySQL, and SQLite3, as well as developing Discord bots and scripting mods for MTA:SA. I also create lightweight applications in C++, which helps me deepen my understanding of performance and optimization. Passionate about problem-solving and continuous learning, I enjoy transforming ideas into functional projects and contributing to innovative technical solutions.`,
+        aboutStats : [
+            {
+                icon : "fa-solid fa-briefcase",
+                title : "Years of Experience :",
+                value : "+5 years"
+            },
+            {
+                icon : "fa-solid fa-diagram-project",
+                title : "Completed Projects :",
+                value : "+50 projects"
+            }
+        ],
+        //Skills
+        skillsTitle : "My Skills",
+        skillsCards : [
+            {
+                icon: "fa-code",
+                title: "Programming languages",
+                description: "JavaScript ,Node.js (Discord.js & Express.js) ,Python ,Lua ,C++"
+            },
+            {
+                icon: "fa-database",
+                title: "Databases",
+                description: "MySQL , MongoDB , SQLite3"
+            },
+        ],
+        //Projects
+        projectsTitle: "My Projects",
+        projectLinkText: "View Project",
+        projectFixText: "Coming Soon",
+        projectsCards:[
+            {
+                image: "https://i.ibb.co/d4Qp0ZVj/image.png",
+                title: "GADZ'IT Platform",
+                description: "A centralized digital platform for ENSAM students, offering easy access to club information, upcoming events, academic resources, and community announcements. Designed to enhance student engagement and simplify campus communication.",
+                link: false,
+            },
+            {
+                image: "https://i.ibb.co/M5swCGJN/image.png",
+                title: "Miami Hotel",
+                description: "The Miami Hotel website is a user-friendly platform designed to simplify the booking process for travelers. With intuitive navigation, real-time availability checks, and exclusive deals, guests can effortlessly reserve their stay at this luxurious Miami-based hotel. Coming soon – your gateway to a perfect vacation!",
+                link: false,
+            },
+            {
+                image: "https://i.ibb.co/Pv9QdCnx/image.png",
+                title: "Grand Theft Auto: The Trilogy - The Definitive Edition Launcher",
+                description: "A streamlined application that lets you quickly launch any of the Definitive Edition games - GTA III, Vice City, or San Andreas - all from one convenient interface. Perfect for fans who want instant access to their favorite classics.",
+                link: false,
+            },
+        ],
+        copyright: "All Rights Reserved",
+    },
+    fr : {
+        home: "Accueil",
+        about: "À Propos",
+        skills: "Mes Compétences",
+        projects: "Mes Projets",
+        heroTitle: "Bonjour, je suis KAICHE ZAAYD",
+        heroSubTitle: "Futur ingénieur logiciel.",
+        //About Me
+        aboutTitle: "À Propos",
+        aboutText: "Je suis un développeur backend et étudiant à l'ENSAM Meknès, spécialisé dans la création de solutions efficaces et évolutives. Mon expérience inclut l'utilisation de Node.js, MongoDB, MySQL et SQLite3, ainsi que le développement de bots Discord et de mods pour MTA:SA. Je crée également des applications légères en C++, ce qui me permet d'approfondir ma compréhension des performances et de l'optimisation. Passionné par la résolution de problèmes et l'apprentissage continu, j'aime transformer des idées en projets fonctionnels et contribuer à des solutions techniques innovantes.",
+        aboutStats: [
+            {
+                icon: "fa-solid fa-briefcase",
+                title: "Années d'expérience :",
+                value: "+5 ans"
+            },
+            {
+                icon: "fa-solid fa-diagram-project",
+                title: "Projets réalisés :",
+                value: "+50 projets"
+            }
+        ],
+        //Skills
+        skillsTitle: "Mes Compétences",
+        skillsCards: [
+            {
+                icon: "fa-code",
+                title: "Langages de programmation",
+                description: "JavaScript, Node.js (Discord.js & Express.js), Python, Lua, C++"
+            },
+            {
+                icon: "fa-database",
+                title: "Bases de données",
+                description: "MySQL, MongoDB, SQLite3"
+            },
+        ],
+        //Projects
+        projectsTitle: "Mes Projets",
+        projectLinkText: "Voir le Projet",
+        projectFixText: "Bientôt disponible",
+        projectsCards:[
+            {
+                image: "https://i.ibb.co/d4Qp0ZVj/image.png",
+                title: "Plateforme GADZ'IT",
+                description: "Une plateforme numérique centralisée pour les étudiants de l'ENSAM, offrant un accès facile aux informations sur les clubs, les événements à venir, les ressources académiques et les annonces communautaires. Conçue pour améliorer l'engagement des étudiants et simplifier la communication sur le campus.",
+                link: false,
+            },
+            {
+                image: "https://i.ibb.co/M5swCGJN/image.png",
+                title: "Miami Hotel",
+                description: "Le site web de Miami Hotel est une plateforme conviviale conçue pour simplifier le processus de réservation pour les voyageurs. Avec une navigation intuitive, des vérifications de disponibilité en temps réel et des offres exclusives, les clients peuvent réserver facilement leur séjour dans cet hôtel luxueux de Miami. Bientôt disponible – votre porte d'entrée vers des vacances parfaites !",
+                link: false,
+            },
+            {
+                image: "https://i.ibb.co/Pv9QdCnx/image.png",
+                title: "Grand Theft Auto: The Trilogy - The Definitive Edition Launcher",
+                description: "Une application simplifiée qui vous permet de lancer rapidement n'importe lequel des jeux de la Definitive Edition - GTA III, Vice City ou San Andreas - le tout depuis une interface pratique. Parfait pour les fans qui veulent un accès instantané à leurs classiques préférés.",
+                link: false,
+            },
+        ],
+        copyright: "Tous droits réservés",
+    }
+}
